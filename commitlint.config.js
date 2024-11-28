4c93f7d80d8ec8724db19c26ff8acf5913605481
Tiago Castro <tiagolobocastro@gmail.com>
2024-11-28 00:07:05 +0000

    security: address github security alerts
    Updates rustls and grpc test package
    
    Signed-off-by: Tiago Castro <tiagolobocastro@gmail.com>

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'example']],
+    'type-enum': [2, 'always', ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'example', 'security']],
     'code-review-rule': [0, 'always'],
   },
   defaultIgnores: false,
